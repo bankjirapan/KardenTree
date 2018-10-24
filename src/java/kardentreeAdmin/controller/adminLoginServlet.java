f55046bcde5067391c60161e3216e01bae94bcce
9banker <jirapan_yankhan@hotmail.com>
2018-10-24 15:03:33 +0700

    [ แก้ไข ] เพิ่มโหลดหน้า jsp เข้ามาและไปดึงข้อมูลจาก DB

--- src/java/kardentreeAdmin/controller/adminLoginServlet.java
+++ src/java/kardentreeAdmin/controller/adminLoginServlet.java
@@ -6,17 +6,30 @@
 package kardentreeAdmin.controller;
 
 import java.io.IOException;
-import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.transaction.UserTransaction;
+import kardentreeAdmin.jpa.controller.AdminLoginJpaController;
+import kardentreeAdmin.jpa.models.AdminLogin;
 
 /**
  *
  * @author bankcom
  */
 public class adminLoginServlet extends HttpServlet {
+
+    @PersistenceUnit(unitName = "KardenTreePU")
+
+    EntityManagerFactory emf;
+
+    @Resource
+    UserTransaction utx;
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -29,22 +42,26 @@ public class adminLoginServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet adminLoginServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet adminLoginServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+
+        String Username = request.getParameter("InUsername");
+        String Password = request.getParameter("InPassword");
+
+        if (Username != null && Password != null) {
+
+            AdminLoginJpaController adminLoginCtrl = new AdminLoginJpaController(utx, emf);
+            AdminLogin adminLoginModel = adminLoginCtrl.findAdminUsername(Username);
+            
+            
+
         }
+        
+        
+
+        
+        getServletContext().getRequestDispatcher("/adminView/adminLoginView.jsp").forward(request, response);
     }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+// <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
