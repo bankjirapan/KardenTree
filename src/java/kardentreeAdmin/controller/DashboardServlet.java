80bfdbb5406f3dc2fc62a75cbcc956d75051f34e
9banker <jirapan_yankhan@hotmail.com>
2018-10-25 18:15:10 +0700

    [ แก้ไข ] โหลดหน้า adminDashboard มาแสดงผล

--- src/java/kardentreeAdmin/controller/DashboardServlet.java
+++ src/java/kardentreeAdmin/controller/DashboardServlet.java
@@ -29,19 +29,9 @@ public class DashboardServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet DashboardServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet DashboardServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+        
+        
+        getServletContext().getRequestDispatcher("/adminView/adminDashboard.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
