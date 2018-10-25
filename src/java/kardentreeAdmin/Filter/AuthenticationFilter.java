1a9009b2a20f8188e0ab70c261d889b5d4a252bf
9banker <jirapan_yankhan@hotmail.com>
2018-10-25 13:59:04 +0700

    [ เพิ่ม ] เพิ่ม filter ของ admin

---
+++ src/java/kardentreeAdmin/Filter/AuthenticationFilter.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeAdmin.Filter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author bankcom
+ */
+public class AuthenticationFilter implements Filter {
+    
+    private FilterConfig config;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        
+        this.config = filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+       
+        
+        HttpSession session = ((HttpServletRequest) request).getSession(false);
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        if(session == null || session.getAttribute("adminLoggedIn") == null){
+            httpResponse.sendRedirect("../admin");
+        } else {
+            chain.doFilter(request, response);
+        }
+        
+        
+    }
+
+    @Override
+    public void destroy() {
+        
+    }
+    
+   
+    
+}
