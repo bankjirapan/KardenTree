47a0ff46aa6ea5cfa10cc34d4147c07e4127e743
Ryan <ryan.@Thanapols-MacBook-Pro.local>
2018-11-01 15:44:20 +0700

    [เพิ่ม] accountLoginSevlet, accountmodel, accountJpa(เพิ่มหาด้วย username)

---
+++ src/java/kardentreeCustomer/jpa/controller/AccountJpaController.java
@@ -0,0 +1,181 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeCustomer.jpa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+import kardentreeCustomer.jpa.controller.exceptions.NonexistentEntityException;
+import kardentreeCustomer.jpa.controller.exceptions.PreexistingEntityException;
+import kardentreeCustomer.jpa.controller.exceptions.RollbackFailureException;
+import kardentreeCustomer.jpa.models.Account;
+
+/**
+ *
+ * @author ryan.
+ */
+public class AccountJpaController implements Serializable {
+
+    public AccountJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Account account) throws PreexistingEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(account);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            if (findAccount(account.getAccountid()) != null) {
+                throw new PreexistingEntityException("Account " + account + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Account account) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            account = em.merge(account);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = account.getAccountid();
+                if (findAccount(id) == null) {
+                    throw new NonexistentEntityException("The account with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Account account;
+            try {
+                account = em.getReference(Account.class, id);
+                account.getAccountid();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The account with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(account);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Account> findAccountEntities() {
+        return findAccountEntities(true, -1, -1);
+    }
+
+    public List<Account> findAccountEntities(int maxResults, int firstResult) {
+        return findAccountEntities(false, maxResults, firstResult);
+    }
+
+    private List<Account> findAccountEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Account.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Account findAccount(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Account.class, id);
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Account findAccountUsername(String username){
+        EntityManager em = getEntityManager();
+        try{
+            Query query = em.createNamedQuery("CustomerName.findByUsername");
+            query.setParameter("username", username);
+            return (Account) query.getSingleResult();
+        }catch (NoResultException NoResult){
+            return null;
+        }finally{
+            em.close();
+        }
+    }
+    
+    
+
+    public int getAccountCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Account> rt = cq.from(Account.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
