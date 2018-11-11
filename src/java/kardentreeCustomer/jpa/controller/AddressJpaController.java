40e98ac943764bd626dfc5b0f3a1a09a513933f9
9banker <jirapan_yankhan@hotmail.com>
2018-11-11 13:25:23 +0700

    [ เพิ่ม ] Auto gen ไฟล์ Address jpa และ model

---
+++ src/java/kardentreeCustomer/jpa/controller/AddressJpaController.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeCustomer.jpa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+import kardentreeCustomer.jpa.controller.exceptions.NonexistentEntityException;
+import kardentreeCustomer.jpa.controller.exceptions.PreexistingEntityException;
+import kardentreeCustomer.jpa.controller.exceptions.RollbackFailureException;
+import kardentreeCustomer.jpa.models.Address;
+
+/**
+ *
+ * @author bankcom
+ */
+public class AddressJpaController implements Serializable {
+
+    public AddressJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Address address) throws PreexistingEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(address);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            if (findAddress(address.getAddressid()) != null) {
+                throw new PreexistingEntityException("Address " + address + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Address address) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            address = em.merge(address);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = address.getAddressid();
+                if (findAddress(id) == null) {
+                    throw new NonexistentEntityException("The address with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Address address;
+            try {
+                address = em.getReference(Address.class, id);
+                address.getAddressid();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The address with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(address);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Address> findAddressEntities() {
+        return findAddressEntities(true, -1, -1);
+    }
+
+    public List<Address> findAddressEntities(int maxResults, int firstResult) {
+        return findAddressEntities(false, maxResults, firstResult);
+    }
+
+    private List<Address> findAddressEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Address.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Address findAddress(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Address.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getAddressCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Address> rt = cq.from(Address.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ src/java/kardentreeCustomer/jpa/models/Address.java
@@ -0,0 +1,104 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeCustomer.jpa.models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author bankcom
+ */
+@Entity
+@Table(name = "ADDRESS")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Address.findAll", query = "SELECT a FROM Address a")
+    , @NamedQuery(name = "Address.findByAddressid", query = "SELECT a FROM Address a WHERE a.addressid = :addressid")
+    , @NamedQuery(name = "Address.findByAccountid", query = "SELECT a FROM Address a WHERE a.accountid = :accountid")
+    , @NamedQuery(name = "Address.findByAddress", query = "SELECT a FROM Address a WHERE a.address = :address")})
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 100)
+    @Column(name = "ADDRESSID")
+    private String addressid;
+    @Size(max = 7)
+    @Column(name = "ACCOUNTID")
+    private String accountid;
+    @Size(max = 1000)
+    @Column(name = "ADDRESS")
+    private String address;
+
+    public Address() {
+    }
+
+    public Address(String addressid) {
+        this.addressid = addressid;
+    }
+
+    public String getAddressid() {
+        return addressid;
+    }
+
+    public void setAddressid(String addressid) {
+        this.addressid = addressid;
+    }
+
+    public String getAccountid() {
+        return accountid;
+    }
+
+    public void setAccountid(String accountid) {
+        this.accountid = accountid;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (addressid != null ? addressid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Address)) {
+            return false;
+        }
+        Address other = (Address) object;
+        if ((this.addressid == null && other.addressid != null) || (this.addressid != null && !this.addressid.equals(other.addressid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "kardentreeCustomer.jpa.models.Address[ addressid=" + addressid + " ]";
+    }
+    
+}
