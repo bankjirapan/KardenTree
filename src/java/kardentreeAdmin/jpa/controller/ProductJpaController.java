7c5616cfa6001e94af6420743d326a49578f5ece
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2018-11-11 19:24:52 +0700

    [เพิ่ม] productJpaController

---
+++ src/java/kardentreeAdmin/jpa/controller/ProductJpaController.java
@@ -0,0 +1,179 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeAdmin.jpa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.NoResultException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+import kardentreeAdmin.jpa.controller.exceptions.NonexistentEntityException;
+import kardentreeAdmin.jpa.controller.exceptions.PreexistingEntityException;
+import kardentreeAdmin.jpa.controller.exceptions.RollbackFailureException;
+import kardentreeAdmin.jpa.models.Product;
+
+/**
+ *
+ * @author ryan.
+ */
+public class ProductJpaController implements Serializable {
+
+    public ProductJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Product product) throws PreexistingEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(product);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            if (findProduct(product.getProductid()) != null) {
+                throw new PreexistingEntityException("Product " + product + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Product product) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            product = em.merge(product);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                String id = product.getProductid();
+                if (findProduct(id) == null) {
+                    throw new NonexistentEntityException("The product with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(String id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Product product;
+            try {
+                product = em.getReference(Product.class, id);
+                product.getProductid();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The product with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(product);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Product> findProductEntities() {
+        return findProductEntities(true, -1, -1);
+    }
+
+    public List<Product> findProductEntities(int maxResults, int firstResult) {
+        return findProductEntities(false, maxResults, firstResult);
+    }
+
+    private List<Product> findProductEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Product.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Product findProduct(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Product.class, id);
+        } finally {
+            em.close();
+        }
+    }
+    
+    public Product findProductName (String productName){
+        EntityManager em = getEntityManager();
+        try{
+            Query query = em.createNamedQuery("Product.findByProductname");
+            query.setParameter("productname", productName);
+            return (Product) query.getSingleResult();
+        }catch (NoResultException NoResult){
+            return null;
+        }finally{
+            em.close();
+        }
+    }
+
+    public int getProductCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Product> rt = cq.from(Product.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
