bf19bc3e54cd240f335a29618cc186bb6e9771a3
9banker <jirapan_yankhan@hotmail.com>
2018-11-11 22:07:06 +0700

    [ แก้ไข ] เปลี่ยนที่อยู่การจัดเก็บ View

--- src/java/kardentreeAdmin/controller/AddProductAdminServlet.java
+++ src/java/kardentreeAdmin/controller/AddProductAdminServlet.java
@@ -75,7 +75,7 @@ public class AddProductAdminServlet extends HttpServlet {
                 return;
             }
         }
-        getServletContext().getRequestDispatcher("/AddproductView.jsp").forward(request, response);
+        getServletContext().getRequestDispatcher("/adminView/adminAddProduct.jsp").forward(request, response);
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
