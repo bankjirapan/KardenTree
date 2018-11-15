ac5ca4e9fdb750d29037472e113aedc59e3b3702
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2018-11-15 14:57:03 +0700

    [เพิ่ม] Gen Category From DB

---
+++ src/java/kardentreeCustomer/jpa/models/Category.java
@@ -0,0 +1,91 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeCustomer.jpa.models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author ryan.
+ */
+@Entity
+@Table(name = "CATEGORY")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Category.findAll", query = "SELECT c FROM Category c")
+    , @NamedQuery(name = "Category.findByCategoryid", query = "SELECT c FROM Category c WHERE c.categoryid = :categoryid")
+    , @NamedQuery(name = "Category.findByCategoryname", query = "SELECT c FROM Category c WHERE c.categoryname = :categoryname")})
+public class Category implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "CATEGORYID")
+    private Integer categoryid;
+    @Size(max = 100)
+    @Column(name = "CATEGORYNAME")
+    private String categoryname;
+
+    public Category() {
+    }
+
+    public Category(Integer categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public Integer getCategoryid() {
+        return categoryid;
+    }
+
+    public void setCategoryid(Integer categoryid) {
+        this.categoryid = categoryid;
+    }
+
+    public String getCategoryname() {
+        return categoryname;
+    }
+
+    public void setCategoryname(String categoryname) {
+        this.categoryname = categoryname;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (categoryid != null ? categoryid.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Category)) {
+            return false;
+        }
+        Category other = (Category) object;
+        if ((this.categoryid == null && other.categoryid != null) || (this.categoryid != null && !this.categoryid.equals(other.categoryid))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "kardentreeCustomer.jpa.models.Category[ categoryid=" + categoryid + " ]";
+    }
+    
+}
