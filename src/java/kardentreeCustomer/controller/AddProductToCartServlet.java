aec4f6599dbad85b519d9f6390dff65ed902bdf0
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2018-11-15 18:45:48 +0700

    [เพิ่ม] AddProductToCartServlet

---
+++ src/java/kardentreeCustomer/controller/AddProductToCartServlet.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeCustomer.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.annotation.Resource;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUnit;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.transaction.UserTransaction;
+import kardentreeAdmin.jpa.controller.ProductJpaController;
+import kardentreeAdmin.jpa.models.Cart;
+import kardentreeAdmin.jpa.models.Product;
+
+/**
+ *
+ * @author ryan.
+ */
+public class AddProductToCartServlet extends HttpServlet {
+    @PersistenceUnit(unitName = "KardenTreePU")
+    EntityManagerFactory emf;
+    
+    @Resource
+    UserTransaction utx;
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession(true);
+        Cart cart = (Cart) session.getAttribute("cart");
+        if(cart == null){
+            cart = new Cart();
+            session.setAttribute("cart", cart);
+        }
+        String productID = request.getParameter("productid");
+        ProductJpaController productJpa = new ProductJpaController(utx,emf);
+        Product p = productJpa.findProduct(productID);
+        cart.add(p);
+        //getServletContext().getRequestDispatcher("/ProductList").forward(request, response);
+        response.sendRedirect("Product");
+    
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
