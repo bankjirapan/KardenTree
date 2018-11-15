6ee8a73320837f1f212b10d5ad23e98355fd13da
Ryan <thanapol.ryan@mail.kmutt.ac.th>
2018-11-15 14:57:43 +0700

    [เพิ่ม] Gen CategroyJpa

---
+++ src/java/kardentreeCustomer/jpa/controller/CategoryJpaController.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kardentreeCustomer.jpa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.transaction.UserTransaction;
+import kardentreeCustomer.jpa.controller.exceptions.NonexistentEntityException;
+import kardentreeCustomer.jpa.controller.exceptions.PreexistingEntityException;
+import kardentreeCustomer.jpa.controller.exceptions.RollbackFailureException;
+import kardentreeCustomer.jpa.models.Category;
+
+/**
+ *
+ * @author ryan.
+ */
+public class CategoryJpaController implements Serializable {
+
+    public CategoryJpaController(UserTransaction utx, EntityManagerFactory emf) {
+        this.utx = utx;
+        this.emf = emf;
+    }
+    private UserTransaction utx = null;
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Category category) throws PreexistingEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            em.persist(category);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            if (findCategory(category.getCategoryid()) != null) {
+                throw new PreexistingEntityException("Category " + category + " already exists.", ex);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Category category) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            category = em.merge(category);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = category.getCategoryid();
+                if (findCategory(id) == null) {
+                    throw new NonexistentEntityException("The category with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws NonexistentEntityException, RollbackFailureException, Exception {
+        EntityManager em = null;
+        try {
+            utx.begin();
+            em = getEntityManager();
+            Category category;
+            try {
+                category = em.getReference(Category.class, id);
+                category.getCategoryid();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The category with id " + id + " no longer exists.", enfe);
+            }
+            em.remove(category);
+            utx.commit();
+        } catch (Exception ex) {
+            try {
+                utx.rollback();
+            } catch (Exception re) {
+                throw new RollbackFailureException("An error occurred attempting to roll back the transaction.", re);
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Category> findCategoryEntities() {
+        return findCategoryEntities(true, -1, -1);
+    }
+
+    public List<Category> findCategoryEntities(int maxResults, int firstResult) {
+        return findCategoryEntities(false, maxResults, firstResult);
+    }
+
+    private List<Category> findCategoryEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Category.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Category findCategory(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Category.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getCategoryCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Category> rt = cq.from(Category.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
